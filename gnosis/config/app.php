734c8ffde94dbf10707dc69139fb01317ea58d10
Kristian Cox <kriss@krisscox.com>
2016-08-31 20:54:35 +0100

    feat: add Twig Bridge to replace blade

--- gnosis/config/app.php
+++ gnosis/config/app.php
@@ -166,8 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
-
-        //
+        TwigBridge\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -224,7 +223,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Twig' => TwigBridge\Facade\Twig::class,
     ],
 
 ];
